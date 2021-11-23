75d1618aa71fa1176aad3abb7ed000451a836f62
kungn103 <kungn103@newschool.edu>
2021-11-23 01:34:53 -0500

    rock garden

--- projects/rock-garden/chatsphere/chatroom/8/main.js
+++ projects/rock-garden/chatsphere/chatroom/8/main.js
@@ -5,28 +5,26 @@ document.onmousemove = function(){
   var x = event.clientX * 100 / window.innerWidth + "%";
   var y = event.clientY * 100 / window.innerWidth + "%";
 
-  for(var i=0;i<6;i++){
+  for(var i=0;i<20;i++){
     balls[i].style.left = x; 
     balls[i].style.top = y; 
     balls[i].style.transform = "translate(-"+x+",-"+y+")";
   }
 }
 
-
 // sound control 
 
 function changeImg() {
     var image = document.getElementById("soundon");
-    if (image.src.match("assets/sound.png")) {
-        image.src = "assets/soundoff.png";
+    if (image.src.match("../assets/sound.png")) {
+        image.src = "../assets/soundoff.png";
     }
     else {
-        image.src = "assets/sound.png";
+        image.src = "../assets/sound.png";
     }
 }
 
-
-var audio = new Audio("assets/doorwhisper.mp3");
+var audio = new Audio("../assets/audio8.mp3");
 
 // sound play automatically on hover 
 document.onmouseover = function() {
@@ -40,7 +38,5 @@ document.getElementById("soundon").addEventListener("click", function (e)
     audio.muted = !audio.muted;
     e.preventDefault();
 }, false);
-
-
 
 
