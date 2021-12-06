c06b30aa116f4b14a9ad317121e3cadb46080df9
kungn103 <kungn103@newschool.edu>
2021-12-06 15:30:24 -0500

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -3,17 +3,15 @@
 function enterName() {
   let text;
   let person = window.prompt("Hi there! What is your name?");
-  if (person == null || person == "") {
-    text = "Oh. Well, you can enter too";
-  } 
-  if (person == "Themis"|| person == "themis") {
-    text = "Welcome home.";
+  if (person == null || person == "" || person == "no" || person == "No") {
+    text = "Oh. Well, you can enter too"; 
   } 
   else {
     text = "Welcome, " + person + "!<br>Please click to enter the Themisverse.";
   }
   document.getElementById("welcome").innerHTML = text;
 }
+
 
 window.onload = enterName()
 
